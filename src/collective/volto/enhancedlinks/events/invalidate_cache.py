08dda2accefa8a9413c1ceace4aedc9b4c4d656f
Mauro Amico <mauro.amico@gmail.com>
2024-03-16 13:06:43 +0100

    tool

--- src/collective/volto/enhancedlinks/events/invalidate_cache.py
+++ src/collective/volto/enhancedlinks/events/invalidate_cache.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-from plone.app.linkintegrity.utils import getIncomingLinks
-from plone.memoize.instance import _m as memojito
+from plone import api
 
 
 def invalidate(obj, event):
@@ -8,8 +7,5 @@ def invalidate(obj, event):
     Invalidate cache for items that has a link to this.
     In this way we will refresh enhanched links infos.
     """
-    direct_links = getIncomingLinks(obj, from_attribute="isReferencing")
-    for link in direct_links:
-        related_obj = link.from_object
-        if related_obj:
-            memojito.clear(related_obj)
+    tool = api.portal.get_tool("portal_enhancedlinks")
+    tool.get_enhanced_link(obj.UID(), force_reload=True)
---
+++ src/collective/volto/enhancedlinks/tool.py
@@ -0,0 +1,55 @@
+from AccessControl import ClassSecurityInfo
+from AccessControl.class_init import InitializeClass
+from OFS.SimpleItem import SimpleItem
+from persistent.mapping import PersistentMapping
+from plone import api
+from Products.CMFCore.utils import UniqueObject
+from zope.interface import implementer
+from zope.interface import Interface
+
+import logging
+
+
+logger = logging.getLogger(__name__)
+_miss = object()
+
+
+class IEnhancedLinksTool(Interface):
+    """Marker interface enhancedlinks tool"""
+
+
+@implementer(IEnhancedLinksTool)
+class EnhancedLinksTool(UniqueObject, SimpleItem):
+    id = "portal_enhancedlinks"
+    meta_type = "Collective Enhanced Links Tool"
+    security = ClassSecurityInfo()
+
+    def __init__(self):
+        self._enhanced_links = PersistentMapping()
+
+    def get_enhanced_link(self, uid, force_reload=False):
+        if not force_reload:
+            info = self._enhanced_links.get(uid, _miss)
+        else:
+            info = _miss
+        if info is _miss:
+            brains = api.content.find(UID=uid, enhanced_links_enabled=True)
+            if not brains:
+                info = {}
+            else:
+                if len(brains) > 1:
+                    logger.warning(f'Found multiple brains with the same UID: "${uid}"')
+                brain = brains[0]
+                if brain.getObjSize == "0 KB":
+                    # not a link to file/image
+                    info = {}
+                else:
+                    info = {
+                        "getObjSize": brain.getObjSize,
+                        "mime_type": brain.mime_type,
+                    }
+        self._enhanced_links[uid] = info
+        return info
+
+
+InitializeClass(EnhancedLinksTool)
